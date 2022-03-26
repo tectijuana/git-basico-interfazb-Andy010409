7b6df680ca2750dce950b5cb6ca098c0ec85e57a
Andrea Perez <99307357+Andy010409@users.noreply.github.com>
2022-03-25 23:13:15 -0700

    Add files via upload

---
+++ Problema 6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+/*
+*Problema 6
+*Introducir las longitudes de los lados de un triángulo encontrar el perímetro
+* //@Autor	Andy010409
+  //@Date  25 Marzo 2022
+*/
+int main()
+{
+
+    int a, b, c, r;
+
+    cout << "-*-*-*-*- Introducir las longitudes de los lados de un triángulo encontrar el perimetro -*-*-*-*-\n";
+
+    cout << "Ingresar el valor del lado a: ";
+    cin >> a;
+    cout << "\nIngresar el valor del lado b: ";
+    cin >> b;
+    cout << "\nIngresar el valor del lado c: ";
+    cin >> c;
+
+    cout << "\nPerimetro:\n";
+    r = a + b + c;
+    cout << r<<"\n"<<endl;
+    return 0;
+
+
+
+}

a6791f2ca816211325175104949c7bff44cf8f09
Andrea Perez <99307357+Andy010409@users.noreply.github.com>
2022-03-26 21:09:33 -0700

    Add files via upload

---
+++ Programa 8-25.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+/*Problema 8/25
+*Generar e imprimir el triángulo de Pascal
+* //@Autor	Andy010409
+  //@Date  26 Marzo 2022
+*/
+int factorial(int a) {
+	int i, s = 1;
+	for (i = 1; i <= a; i++) {
+		s = s * i;
+	}
+	return s;
+
+}
+
+int combinacion(int a, int b) {
+	return factorial(a) / (factorial(b) * factorial(a - b));
+}
+int main()
+{
+	int i, j, n = 7;
+	for (i = 0; i <= n; i++) {
+		for (j = n-i; j >=0; j--) {
+			cout << " ";
+		}
+		for (j = 0; j <=i; j++) {
+			cout << combinacion(i,j)<<" ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
+
